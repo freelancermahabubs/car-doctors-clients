01925a2de19cdfd68af4bd219200aeae09f23039
MAHABUB <info.freelancermahabub@gmail.com>
2023-05-22 20:04:26 +0600

    added serch

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,14 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [asc, setAsc] = useState(true);
   const [services, setServices] = useState([]);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    fetch(`http://localhost:5000/carsServices?sort=${asc ? "asc" : "desc"}`)
+    fetch(
+      `http://localhost:5000/carsServices?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [asc]);
+  }, [asc, search]);
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+    setSearch(searchRef.current.value);
+  };
   return (
     <div className="mt-4">
       <div className="text-center space-y-3">
@@ -19,6 +29,32 @@ const Services = () => {
           humour, or randomised <br /> words which dont look even slightly
           believable.
         </p>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              ref={searchRef}
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
         <button onClick={() => setAsc(!asc)} className="btn btn-primary">
           {asc ? "Price High to Low" : "Price Low To high"}
         </button>
