2636c622d5ddedd7b847e1c99906ea27941d0e69
MAHABUB <info.freelancermahabub@gmail.com>
2023-05-22 19:15:09 +0600

    sorting by price

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
+  const [asc, setAsc] = useState(true);
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("https://y-five-alpha.vercel.app/carsServices")
+    fetch(`http://localhost:5000/carsServices?sort=${asc ? "asc" : "desc"}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc]);
   return (
     <div className="mt-4">
       <div className="text-center space-y-3">
@@ -18,6 +19,9 @@ const Services = () => {
           humour, or randomised <br /> words which dont look even slightly
           believable.
         </p>
+        <button onClick={() => setAsc(!asc)} className="btn btn-primary">
+          {asc ? "Price High to Low" : "Price Low To high"}
+        </button>
       </div>
       <div className="grid lg:grid-cols-3 mt-5 md:grid-cols-2 sm:grid-cols-1 gap-5">
         {services.map((service) => (
